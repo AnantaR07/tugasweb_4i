cf2a87725bd02c37ee6c29858d1c6d910c645516
Ananta Romadhan <80764638+AnantaR07@users.noreply.github.com>
2022-04-09 02:10:06 +0700

    Add files via upload

---
+++ tugasweb/main/index.php
@@ -0,0 +1,161 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>N.A.N</title>
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+    <link rel="stylesheet" href="Style.css">
+<body>
+
+	<div class="Background"></div>
+
+        <div class="TextName">
+    	<p>Ananta Romadhan</p>
+    	<h2>Desainer Web Pemula</h2>
+    </div>
+
+    <div class="content">
+    <img src="My.png" alt="">
+	</div>
+
+    <div class="TextName2">
+    	<h2>Hai, perkenalkan nama saya Ananta Romadhan. Saya </br> adalah Mahasiswa dari Universitas Muhammadiyah </br> Malang (UMM), dan saya masih seorang desainer web </br> pemula(⌐▨_▨)</h2>
+    </div>
+		
+	<div class="content2">
+    <div class="ImagePortofolio"></div>
+	</div>
+
+	<div class="Name">
+		<h2> Ananta Romadhan</h2>
+	</div>
+
+	<div class="Slogan">
+		<h2> "pusing Sudah Jadi Keseharian"</h2>
+	</div>
+
+	<div class="Karakter">
+		<h2> Karakter</h2>
+	</div>
+			<div class="ket1">
+				<h2> > Baik   </br>
+					 > Sopan  </br>
+					 > Bertanggung Jawab </br>
+					 > Dapat Memahami Suatu Keadaan</br>
+					 > Disiplin
+				</h2>
+			</div>
+
+	<div class="Pengalaman">
+		<h2> Pengalaman</h2>
+	</div>
+
+			<div class="ket2">
+				<h2> Pengalaman Kerja </h2>
+			</div>
+
+			<div class="ket2-1">
+				<h2> Masih Belum Memilki Pengalaman Kerja</h2>
+			</div>
+
+	<div class="Pendidikan">
+		<h2> Pendidikan</h2>
+	</div>
+
+			<div class="ket3">
+				<h2> Formal </h2>
+			</div>
+
+			<div class="ket3-1">
+				<h2> 1. Universitas Muhammadiyah Malang</br>
+					 2. SMAN 1 Durenan</br>
+					 3. SMPN 1 Durenan</br>
+				     4. SDN 1 Kendal Rejo
+				</h2>
+			</div>
+
+	<div class="Prestasi">
+		<h2> Prestasi</h2>
+	</div>
+
+			<div class="ket4">
+				<h2> > Membuat Konsep Web Jual Beli Makanan </br>
+					 > Membuat Apliaksi Catatan </br>
+					 > Membuat Apliaksi Alarm
+				</h2>
+			</div>
+
+	<div class="Kemampuan">
+		<h2> Kemampuan</h2>
+	</div>
+
+	<div class="SocialMedia">
+		<h2> Watashiwa Kemplenk </br></br></br> 
+			 anan_7753 </br></br></br></br> 
+			 AnantaR07
+		</h2>
+	</div>
+
+	<div class="boxright"></div>
+	 <button>Download Portofolio</button>
+
+<div class="ket5">
+<div class="skills__containter container grid">
+                <div>
+                    <div class="skillscontent">
+                        <div class="skillsheader">
+                            <i class="uil uil-brackets-curly skillsicon"></i>
+
+                                <i class="uil uil-angle-down skillsarrow"></i>
+                        </div>
+
+                        <div class="skillslist grid">
+                            <div class="skillsdata">
+                                <div class="skillstitles">
+                                    <h3 class="skillsname">C/C++</h3>
+                                    <div class="skillsnumber"><h2>75%</h2></div>
+                                </div>
+                                <div class="skillsbar">
+                                    <div class="skillsbar2"></div>
+                                </div>
+                            </div>
+
+                            <div class="skillsdata">
+                                 <div class="skillstitles">
+                                     <h3 class="skillsname">CSS</h3>
+                                     <div class="skillsnumber"><h2>85%</h2></div>
+                                 </div>
+                                 <div class="skillsbar">
+                                     <div class="skillsbar2"></div>
+                                 </div>
+                            </div>
+
+                            <div class="skillsdata">
+                                <div class="skillstitles">
+                                    <h3 class="skillsname">Java</h3>
+                                    <div class="skillsnumber"><h2>75%</h2></div>
+                                </div>
+                                <div class="skillsbar">
+                                    <div class="skillsbar2"></div>
+                                </div>
+                           </div>
+
+                           <div class="skills__data">
+                                 <div class="skillstitles">
+                                     <h3 class="skillsname">PHP</h3>
+                                     <div class="skillsnumber"><h2>70%</h2></div>
+                                 </div>
+                                 <div class="skillsbar">
+                                     <div class="skillsbar2"></div>
+                                 </div>
+                            </div>
+                            </div>
+                        </div>
+                  </div>
+            </div>
+        </div>
+     </head>
+</body>
+</html>
